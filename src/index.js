5b82297b0887d5245f6040ec543821d47a104827
David Cruz <jordav_7@hotmail.com>
2017-12-06 23:38:33 -0500

    Se resuelve problema con plugin jquery.extensions

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './base/App';
 import registerServiceWorker from './registerServiceWorker';
-import $ from 'jquery/dist/jquery.min';
+import $ from 'jquery';
 //import $ from 'jquery';
-window.jQuery = $;
-window.$ = $;
+window.jQuery = window.$ = $;
 global.jQuery = $;
 //js
+require('jquery.inputmask/dist/jquery.inputmask.bundle');
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
