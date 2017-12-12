da695d334638a1bf5bf05de1630645b44ca80a1c
David Cruz <jordav_7@hotmail.com>
2017-12-12 17:37:48 -0500

    Cambio a menu nativo

---
+++ src/base/theme/sidebar/TreeView.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+
+export default class TreeView extends Component {
+
+    static defaultProps = {
+        
+    }
+
+    static propTypes = {
+        
+    }
+
+    render() {
+        return (
+            <li className="treeview">
+                <a >
+                    <i className="fa fa-share"></i> <span>Multilevel</span>
+                    <span className="pull-right-container">
+                        <i className="fa fa-angle-left pull-right"></i>
+                    </span>
+                </a>
+                <ul className="treeview-menu">
+                    <li><a ><i className="fa fa-circle-o"></i> Level One</a></li>
+                    <li className="treeview">
+                    <a >
+                        <i className="fa fa-circle-o"></i> Level One
+                        <span className="pull-right-container">
+                            <i className="fa fa-angle-left pull-right"></i>
+                        </span>
+                    </a>
+                    <ul className="treeview-menu">
+                        <li>
+                            <a >
+                                <i className="fa fa-circle-o"></i> Level Two
+                            </a>
+                        </li>
+                        <li className="treeview">
+                            <a >
+                                <i className="fa fa-circle-o"></i> Level Two
+                                <span className="pull-right-container">
+                                    <i className="fa fa-angle-left pull-right"></i>
+                                </span>
+                            </a>
+                            <ul className="treeview-menu">
+                                <li><a ><i className="fa fa-circle-o"></i> Level Three</a></li>
+                                <li><a ><i className="fa fa-circle-o"></i> Level Three</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    </li>
+                    <li><a ><i className="fa fa-circle-o"></i> Level One</a></li>
+                </ul>
+            </li>
+        )
+    }
+}
