c4cf583cd9f0e9d86de42c5537a7d8dc89d2bbad
David Cruz <jordav_7@hotmail.com>
2018-01-17 07:33:04 -0500

    cambios sin ependencias de terceros

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './base/App';
 import registerServiceWorker from './registerServiceWorker';
-import $ from 'jquery';
-window.jQuery = window.$ = $;
-global.jQuery = $;
+//import $ from 'jquery';
+//window.jQuery = window.$ = $;
+//global.jQuery = $;
 //js
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
 /*require('jquery.inputmask/dist/jquery.inputmask.bundle');*/
-require('bootstrap');
+//require('bootstrap');
 /*require('bootstrap-daterangepicker');
 require('bootstrap-datepicker');
 require('bootstrap-colorpicker');
